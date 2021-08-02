e829b99fa1de63aa210dcb051c5589533ca83557
Abul Kalam Azad <dev.akazad@hotmail.com>
2021-08-02 23:32:05 +0600

    map info content added along with zip code search

--- js/main.js
+++ js/main.js
@@ -1,242 +1,542 @@
 // Dummy locations data
 
 const locations = [
-    {
-        lat: '42.9298847',
-        lng: '-78.9186068'
-    },
-    {
-        lat: '51.07492',
-        lng: '-114.002812'
-    },
-    {
-        lat: '20.12586478230905',
-        lng: '78.69799291354367',
-    },
-    {
-        lat: '21.12586478230905',
-        lng: '77.69799291354367',
-    },
-    {
-        lat: '20.22586478230905',
-        lng: '78.79799291354367',
-    },
-    {
-        lat: '23.98403001768207',
-        lng: '54.71326588103569',
-    },
-    {
-        lat: '25.372430252828085',
-        lng: '44.1337424217921',
-    },
-    {
-        lat: '33.195345871145975',
-        lng: '44.09975543981851',
-    },
-    {
-        lat: '31.863803668294945',
-        lng: '34.94741250760682',
-    },
-    {
-        lat: '21.22285643773062',
-        lng: '56.68777764267978',
-    },{
-        lat: '47.4893487040078',
-        lng: '14.267037498434016',
-    },
-    {
-        lat: '53.81509236445907',
-        lng: '27.711424000970755',
-    },
-    {
-        lat: '50.73967115576341',
-        lng: '4.90516371514053',
-    },
-    {
-        lat: '44.15592415160172',
-        lng: '18.002137235820676',
-    },
-    {
-        lat: '42.687337177889106',
-        lng: '25.32164688368282',
-    },
-    {
-        lat: '45.63597032223503',
-        lng: '16.042139789709775',
-    },
-    {
-        lat: '35.07764967634815',
-        lng: ' 33.28223069526854',
-    },
-    {
-        lat: '49.831750478828035',
-        lng: '15.78109991737886',
-    },
-    {
-        lat: '56.15476928448669',
-        lng: '10.201181123414475',
-    },
-    {
-        lat: '58.595174752173826',
-        lng: '25.90908212632772',
-    },
-    {
-        lat: '36.194723',
-        lng: '43.977207'
-    },
-    {
-        lat: '32.0678053',
-        lng: '34.789773'
-    },
-    {
-        lat: '23.590661',
-        lng: '58.373874'
-    },
-    {
-        lat: '25.1877643',
-        lng: '55.2683964'
-    },
-    {
-        lat: '47.2793581',
-        lng: '11.4714138'
-    },
-    {
-        lat: '47.520435',
-        lng: '12.4313879'
-    },
-    {
-        lat: '48.1500467',
-        lng: '13.9826652'
-    },
-    {
-        lat: '47.1529188',
-        lng: '9.8150949'
-    },
-    {
-        lat: '47.4914445',
-        lng: '9.7267561'
-    },
-    {
-        lat: '47.4728132',
-        lng: '11.8772564'
-    },
-    {
-        lat: '48.1164095',
-        lng: '15.6106072'
-    },
-    {
-        lat: '47.1962126',
-        lng: '15.3568231'
-    },
-    {
-        lat: '48.258508',
-        lng: '16.407274'
-    },
-    {
-        lat: '48.2939586',
-        lng: '13.3994531'
-    },
-    {
-        lat: '48.2089358',
-        lng: '13.4861551'
-    },
-    {
-        lat: '47.014187',
-        lng: '15.210316'
-    },
-    {
-        lat: '61.3681996',
-        lng: '23.8007864'
-    },
-    {
-        lat: '60.9344445',
-        lng: '25.6247219'
-    },
-    {
-        lat: '60.4793192',
-        lng: '22.1812849'
-    },
-    {
-        lat: '64.0624367',
-        lng: '24.5629238'
-    },
-    {
-        lat: '60.2826123',
-        lng: '24.822004'
-    },
+  {
+    lat: '42.9298847',
+    lng: '-78.9186068',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '01',
+    zoom: 12,
+  },
+  {
+    lat: '51.07492',
+    lng: '-114.002812',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '00',
+    zip: '02',
+    zoom: 12,
+  },
+  {
+    lat: '20.12586478230905',
+    lng: '78.69799291354367',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '03',
+    zoom: 12,
+  },
+  {
+    lat: '21.12586478230905',
+    lng: '77.69799291354367',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '04',
+    zoom: 12,
+  },
+  {
+    lat: '20.22586478230905',
+    lng: '78.79799291354367',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '05',
+    zoom: 12,
+  },
+  {
+    lat: '23.98403001768207',
+    lng: '54.71326588103569',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '06',
+    zoom: 9,
+  },
+  {
+    lat: '25.372430252828085',
+    lng: '44.1337424217921',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '07',
+    zoom: 12,
+  },
+  {
+    lat: '33.195345871145975',
+    lng: '44.09975543981851',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '08',
+    zoom: 9,
+  },
+  {
+    lat: '31.863803668294945',
+    lng: '34.94741250760682',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '09',
+    zoom: 12,
+  },
+  {
+    lat: '21.22285643773062',
+    lng: '56.68777764267978',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '10',
+    zoom: 12,
+  },
+  {
+    lat: '47.4893487040078',
+    lng: '14.267037498434016',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '11',
+    zoom: 12,
+  },
+  {
+    lat: '53.81509236445907',
+    lng: '27.711424000970755',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '12',
+    zoom: 12,
+  },
+  {
+    lat: '50.73967115576341',
+    lng: '4.90516371514053',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '13',
+    zoom: 12,
+  },
+  {
+    lat: '44.15592415160172',
+    lng: '18.002137235820676',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '14',
+    zoom: 12,
+  },
+  {
+    lat: '42.687337177889106',
+    lng: '25.32164688368282',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '15',
+    zoom: 12,
+  },
+  {
+    lat: '45.63597032223503',
+    lng: '16.042139789709775',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '16',
+    zoom: 12,
+  },
+  {
+    lat: '35.07764967634815',
+    lng: ' 33.28223069526854',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '17',
+    zoom: 12,
+  },
+  {
+    lat: '49.831750478828035',
+    lng: '15.78109991737886',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '18',
+    zoom: 12,
+  },
+  {
+    lat: '56.15476928448669',
+    lng: '10.201181123414475',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '19',
+    zoom: 12,
+  },
+  {
+    lat: '58.595174752173826',
+    lng: '25.90908212632772',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '20',
+    zoom: 12,
+  },
+  {
+    lat: '36.194723',
+    lng: '43.977207',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '21',
+    zoom: 12,
+  },
+  {
+    lat: '32.0678053',
+    lng: '34.789773',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '22',
+    zoom: 12,
+  },
+  {
+    lat: '23.590661',
+    lng: '58.373874',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '23',
+    zoom: 12,
+  },
+  {
+    lat: '25.1877643',
+    lng: '55.2683964',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '24',
+    zoom: 12,
+  },
+  {
+    lat: '47.2793581',
+    lng: '11.4714138',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '25',
+    zoom: 12,
+  },
+  {
+    lat: '47.520435',
+    lng: '12.4313879',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '26',
+    zoom: 12,
+  },
+  {
+    lat: '48.1500467',
+    lng: '13.9826652',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '27',
+    zoom: 12,
+  },
+  {
+    lat: '47.1529188',
+    lng: '9.8150949',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '28',
+    zoom: 12,
+  },
+  {
+    lat: '47.4914445',
+    lng: '9.7267561',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '29',
+    zoom: 12,
+  },
+  {
+    lat: '47.4728132',
+    lng: '11.8772564',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '30',
+    zoom: 12,
+  },
+  {
+    lat: '48.1164095',
+    lng: '15.6106072',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '31',
+    zoom: 12,
+  },
+  {
+    lat: '47.1962126',
+    lng: '15.3568231',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '32',
+    zoom: 12,
+  },
+  {
+    lat: '48.258508',
+    lng: '16.407274',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '33',
+    zoom: 12,
+  },
+  {
+    lat: '48.2939586',
+    lng: '13.3994531',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '34',
+    zoom: 12,
+  },
+  {
+    lat: '48.2089358',
+    lng: '13.4861551',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '35',
+    zoom: 12,
+  },
+  {
+    lat: '47.014187',
+    lng: '15.210316',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '36',
+    zoom: 12,
+  },
+  {
+    lat: '61.3681996',
+    lng: '23.8007864',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '37',
+    zoom: 12,
+  },
+  {
+    lat: '60.9344445',
+    lng: '25.6247219',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '38',
+    zoom: 12,
+  },
+  {
+    lat: '60.4793192',
+    lng: '22.1812849',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '39',
+    zoom: 12,
+  },
+  {
+    lat: '64.0624367',
+    lng: '24.5629238',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '40',
+    zoom: 12,
+  },
+  {
+    lat: '60.2826123',
+    lng: '24.822004',
+    name: 'Supplier X',
+    email: 'contact@supplierx.com',
+    phone: '01799871879',
+    address: 'East Dendabar, Kabarsthan road',
+    zip: '41',
+    zoom: 12,
+  },
 ];
 
+// Referrence to form element
+
+const mapForm = document.querySelector('.map-input-form');
 
 // Initialize the map with cluster
 
-function initMap() {
-    
-    // Map Options
-    
-    const mapOptions = {
-        center: { lat: 23.27020396438825, lng: 13.267567365815008 },
-        zoom: 2,
-        disableDefaultUI: false,
-    };
-    
-    // Map Element
+function initMap(zoom = 2, lat = 23.27020396438825, lng = 13.267567365815008) {
+  // Map Options
 
-    const mapElement = document.getElementById('map');
+  const mapOptions = {
+    center: { lat: parseFloat(lat), lng: parseFloat(lng) },
+    zoom: zoom,
+    disableDefaultUI: false,
+  };
 
-    // Calling the map constructor
+  // Map Element
 
-    const map = new google.maps.Map(mapElement, mapOptions);
+  const mapElement = document.getElementById('map');
 
-    const markers = locations.map(({ lat, lng }) => {
-        return new google.maps.Marker({
-          position: { lat: parseFloat(lat), lng: parseFloat(lng) },
-          icon: "../img/gulf.png"
-        });
+  // Calling the map constructor
+
+  const map = new google.maps.Map(mapElement, mapOptions);
+
+  const markers = locations.map(({ lat, lng, name, phone, email, address }) => {
+    // Info window content
+    const contentString =
+      '<div id="content">' +
+      `<h2 id="firstHeading" class="firstHeading">${name}</h2>` +
+      '<div id="bodyContent">' +
+      `<p class="phone">Phone: ${phone}</p>` +
+      `<p class="email">Email:${email}</p>` +
+      `<p class="address">Address:${address}</p>` +
+      '</div>' +
+      '</div>';
+
+    // Initialing info content
+    const infowindow = new google.maps.InfoWindow({
+      content: contentString,
     });
 
-    const markerClusterOptions = {
-        styles: [
-            {   
-            anchorText: [ 20, -1 ],
-            textColor: 'white',
-            height: 53,
-            width: 53,
-            url: "../img/ClusterIcon.png",
-            },
-            {
-            anchorText: [ 20, -2 ],
-            textColor: 'white',
-            height: 56,
-            width: 56,
-            url: "../img/ClusterIcon.png",
-            },
-            {
-            anchorText: [ 20, -7.5 ],
-            textColor: 'white',
-            height: 66,
-            width: 66,
-            url: "../img/ClusterIcon.png",
-            },
-            {
-            anchorText: [ 20, -13 ],
-            textColor: 'white',
-            height: 78,
-            width: 78,
-            url: "../img/ClusterIcon.png",
-            },
-            {
-            anchorText: [ 20, -18.5 ],
-            textColor: 'white',
-            height: 90,
-            width: 90,
-            url: "../img/ClusterIcon.png",
-            }
-        ]
-    }
-    
-    new MarkerClusterer(
+    // Initializing marker
+    const marker = new google.maps.Marker({
+      position: { lat: parseFloat(lat), lng: parseFloat(lng) },
+      icon: '../img/gulf.png',
+    });
+
+    // Creating add event listener to each marker
+    marker.addListener('click', () => {
+      infowindow.open({
+        anchor: marker,
         map,
-        markers,
-        markerClusterOptions
-    );
+        shouldFocus: true,
+      });
+    });
+
+    // Returning marker
+    return marker;
+  });
+
+  const markerClusterOptions = {
+    styles: [
+      {
+        anchorText: [20, -1],
+        textColor: 'white',
+        height: 53,
+        width: 53,
+        url: '../img/ClusterIcon.png',
+      },
+      {
+        anchorText: [20, -2],
+        textColor: 'white',
+        height: 56,
+        width: 56,
+        url: '../img/ClusterIcon.png',
+      },
+      {
+        anchorText: [20, -7.5],
+        textColor: 'white',
+        height: 66,
+        width: 66,
+        url: '../img/ClusterIcon.png',
+      },
+      {
+        anchorText: [20, -13],
+        textColor: 'white',
+        height: 78,
+        width: 78,
+        url: '../img/ClusterIcon.png',
+      },
+      {
+        anchorText: [20, -18.5],
+        textColor: 'white',
+        height: 90,
+        width: 90,
+        url: '../img/ClusterIcon.png',
+      },
+    ],
+  };
+
+  new MarkerClusterer(map, markers, markerClusterOptions);
 }
+
+// Finding lat and lng
+
+function findLatLng(zipcode) {
+  let item;
+
+  item = locations.filter(({ zip }) => parseInt(zip) === parseInt(zipcode));
+
+  if (item.length !== 0) {
+    initMap(item[0].zoom, item[0].lat, item[0].lng);
+  } else {
+    initMap(2, 23.27020396438825, 13.267567365815008);
+  }
+}
+
+// Handling map form submit
+
+mapForm.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (e.target.children[0].value !== '') {
+    findLatLng(e.target.children[0].value);
+    e.target.children[0].value = '';
+  }
+});
